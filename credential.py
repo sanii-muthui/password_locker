a2da3819177cc5a82d0e7c8f572d6d1405591295
sanii-muthui <muthuisanii@gmail.com>
2019-07-22 12:30:13 +0300

    class naming and initialization

---
+++ credential.py
@@ -0,0 +1,23 @@
+class Credential:
+    """
+    a class that generates new credential for users
+    """
+    pass
+    credential_array = []
+
+    def __init__(self, password, email):
+        self.email = email
+        self.password = password
+
+    def save_credential(self):
+        """
+        save_contact method saves credentials objects into credential_array
+        """
+        Credential.credential_array.append(self)
+
+    @classmethod
+    def display_credential(cls):
+        """
+        method that returns the credential array
+        """
+        return cls.credential_array
